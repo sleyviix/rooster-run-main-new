8e45729d33bb7cf6728c7c556d21fe07570b921a
suleman2020 <190028486@aston.ac.uk>
2021-03-08 18:54:00 +0000

    update MultiplayerMenuScreen.java and SoundManager.java
    Bug fixes

--- core/src/uk/ac/aston/teamproj/game/scenes/SoundManager.java
+++ core/src/uk/ac/aston/teamproj/game/scenes/SoundManager.java
@@ -11,11 +11,11 @@ public class SoundManager implements Sound {
 
 
 	public static final Sound POP = Gdx.audio.newSound(Gdx.files.internal("pop.mp3"));
-	public static final Sound CLICK = Gdx.audio.newSound(Gdx.files.internal("click.mp3"));
+	public static final Sound CLICK = Gdx.audio.newSound(Gdx.files.internal("menu_click.mp3"));
 	public static final Sound LIGHTENING = Gdx.audio.newSound(Gdx.files.internal("lightening.mp3"));
 	public static final Sound GAMEOVER = Gdx.audio.newSound(Gdx.files.internal("gameover.mp3"));
 	public static final Sound FIRSTPLACE = Gdx.audio.newSound(Gdx.files.internal("firstplace.wav"));
-	public static final Sound SWOOSH = Gdx.audio.newSound(Gdx.files.internal("electric-transition-super-quick-wwww.mp3"));
+	public static final Sound SWOOSH = Gdx.audio.newSound(Gdx.files.internal("electric-transition-super-quick-www.mp3"));
 	public static final Sound COIN = Gdx.audio.newSound(Gdx.files.internal("coin.wav"));
 	public static final Sound BOMB = Gdx.audio.newSound(Gdx.files.internal("bomb.wav"));
 
